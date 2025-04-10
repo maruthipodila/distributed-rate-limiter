291d0917f5b1c80b6a56910c02722f9d8eabf1cd
Maruthi Podila <maruthi.podila@bright.blue>
2025-04-10 15:30:25 +0530

    Redis connection

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,38 @@
-// main.go
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
-	log.Println("Server is running on http://localhost:8081")
-	err := http.ListenAndServe(":8080", nil)
+	// Connect to Redis
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // No password
+		DB:       0,  // Default DB
+		Protocol: 2,  // RESP2
+	})
+
+	ctx := context.Background()
+	err := client.Set(ctx, "foo", "bar", 0).Err()
+	if err != nil {
+		log.Fatalf("Redis SET failed: %v", err)
+	}
+
+	val, err := client.Get(ctx, "foo").Result()
+	if err != nil {
+		log.Fatalf("Redis GET failed: %v", err)
+	}
+	fmt.Println("foo:", val)
+
+	// Start HTTP server
+	log.Println("Server is running on http://localhost:8080")
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
