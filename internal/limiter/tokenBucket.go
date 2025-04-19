4a22e7f6ea8811069d3e89fbc6c05b2cc195c348
Maruthi Podila <maruthi.podila@bright.blue>
2025-04-20 00:01:30 +0530

    Struct field names modifications

--- internal/limiter/tokenBucket.go
+++ internal/limiter/tokenBucket.go
@@ -1,23 +1,25 @@
 package limiter
 
+import "time"
+
 type TokenBucket struct {
 	// Maximum number of tokens in the bucket
-	Capacity int 
+	capacity int 
 	// Rate at which tokens are added to the bucket
-	Rate     int 
+	refillRate int 
 	// Tokens is the current number of tokens in the bucket
-	Tokens int
+	tokensCounter int
 	// LastRefill is the last time the bucket was refilled
-	LastRefill int64
+	lastRefillTime time.Time
 }
 
 // NewTokenBucket creates a new TokenBucket with the given capacity and rate
 func NewTokenBucket(capacity, rate int) *TokenBucket {
 	return &TokenBucket{
-		Capacity:  capacity,
-		Rate:      rate,
-		Tokens:    capacity,
-		LastRefill: 0,
+		capacity: capacity,
+		refillRate: rate,
+		tokensCounter: capacity,
+		lastRefillTime: time.Now(),
 	}
 }
 
