e80afbb24bbf07548a43983412826e9db8cdf4a6
Maruthi Podila <maruthi.podila@bright.blue>
2025-04-20 00:13:10 +0530

    More changes in bucket struct type

--- internal/limiter/tokenBucket.go
+++ internal/limiter/tokenBucket.go
@@ -5,25 +5,25 @@ import "time"
 type TokenBucket struct {
 	// Maximum number of tokens in the bucket
 	capacity int 
-	// Rate at which tokens are added to the bucket
-	refillRate int 
-	// Tokens is the current number of tokens in the bucket
+	// refill interval in seconds
+	refillInterval time.Duration	
+	// Current number of tokens in the bucket
 	tokensCounter int
 	// LastRefill is the last time the bucket was refilled
 	lastRefillTime time.Time
 }
 
 // NewTokenBucket creates a new TokenBucket with the given capacity and rate
-func NewTokenBucket(capacity, rate int) *TokenBucket {
+func NewTokenBucket(capacity int, refillInterval time.Duration) *TokenBucket {
 	return &TokenBucket{
 		capacity: capacity,
-		refillRate: rate,
+		refillInterval: refillInterval,
 		tokensCounter: capacity,
 		lastRefillTime: time.Now(),
 	}
 }
 
-func (TokenBucket) Allow(key string) bool {
+func (bucket *TokenBucket) Allow(key string) bool {
 	// Check if the token bucket has enough tokens
 	// If yes, consume a token and return true
 	// If no, return false
